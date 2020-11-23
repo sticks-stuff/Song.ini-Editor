01016ce3aeb4b70e43e9f11a65ade9c48bea527a
Thestickman391 <stickman391@gmail.com>
2020-11-23 17:50:21 +1300

    Selection stuff

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
@@ -42,11 +43,11 @@
             // 
             // button1
             // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.button1.Location = new System.Drawing.Point(704, 0);
+            this.button1.Location = new System.Drawing.Point(608, 199);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(96, 450);
+            this.button1.Size = new System.Drawing.Size(189, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "LOAD .ini";
             this.button1.UseVisualStyleBackColor = true;
@@ -55,7 +56,8 @@
             // dataGridView1
             // 
             this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
@@ -64,16 +66,23 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(674, 426);
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.DefaultCellStyle.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.Size = new System.Drawing.Size(590, 426);
             this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellBeginEdit += new System.Windows.Forms.DataGridViewCellCancelEventHandler(this.dataGridView1_CellBeginEdit);
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
+            this.dataGridView1.ColumnHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_ColumnHeaderMouseClick);
+            this.dataGridView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataGridView1_KeyDown);
             // 
             // button2
             // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.button2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.button2.Dock = System.Windows.Forms.DockStyle.Right;
-            this.button2.Location = new System.Drawing.Point(608, 0);
+            this.button2.Location = new System.Drawing.Point(608, 242);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(96, 450);
+            this.button2.Size = new System.Drawing.Size(189, 23);
             this.button2.TabIndex = 3;
             this.button2.Text = "EDIT ALL";
             this.button2.UseVisualStyleBackColor = true;
@@ -97,8 +106,8 @@
         #endregion
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,23 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls.Primitives;
 using System.Windows.Forms;
+using ButtonBase = System.Windows.Controls.Primitives.ButtonBase;
 
 namespace Song.ini_Editor
 {
     public partial class Form1 : Form
     {
+
         public static DataTable dt = new DataTable();
         public Form1()
         {
             InitializeComponent();
-            
         }
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -33,6 +32,9 @@ namespace Song.ini_Editor
         {
             int size = -1;
             this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.Filter = "Song.ini Files|song.ini|All files (*.*)|*.*";
+            this.openFileDialog1.FileName = "song.ini";
+
             DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
             if (result == DialogResult.OK) // Test result.
             {
@@ -41,9 +43,8 @@ namespace Song.ini_Editor
                 if (dt2.Columns.Contains("File name") == false)
                 {
                     dt2.Columns.Add("File name");
+                    dt2.Columns[0].ReadOnly = true;
                 }
-                Console.WriteLine(openFileDialog1.FileNames[0]);
-                Console.WriteLine(openFileDialog1.FileNames[1]);
                 try
                 {
                     foreach (string file in openFileDialog1.FileNames) 
@@ -56,7 +57,8 @@ namespace Song.ini_Editor
                         {
                             string[] data = line.Split('\n');
                             //Console.WriteLine(data[0]);
-                            if (data[0].Contains("=")) {
+                            if (data[0].Contains("=")) 
+                            {
                                 var inputName = data[0].Split(" = ".ToCharArray()).First();
                                 var inputData = data[0].Substring(data[0].LastIndexOf("=") + 1);
                                 Console.WriteLine(inputName);
@@ -77,7 +79,12 @@ namespace Song.ini_Editor
             }
             Console.WriteLine(size); // <-- Shows file size in debugging mode.
             Console.WriteLine(result); // <-- For debugging use.
-            
+
+            for (int i = 0; i < dataGridView1.Columns.Count; i++)
+            {
+                dataGridView1.Columns[i].SortMode = DataGridViewColumnSortMode.NotSortable;
+            }
+
         }
 
         private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
@@ -88,6 +95,60 @@ namespace Song.ini_Editor
         private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
         {
             
+        }
+        private void dataGridView1_ColumnHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (Control.ModifierKeys != Keys.Shift)
+            {
+                dataGridView1.ClearSelection();
+            }
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                dataGridView1.CurrentCell = dataGridView1[e.ColumnIndex, 0];
+                dataGridView1[e.ColumnIndex, i].Selected = true;
+            }
+        }
+
+        private int _keyValue;
+        private Boolean _checkKeyValue = false;
+        private int value;
+
+        private void dataGridView1_CellBeginEdit(object sender, DataGridViewCellCancelEventArgs e)
+        {
+            DataGridViewCell cell = dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex];
+
+            if (_checkKeyValue)
+            {
+                _checkKeyValue = false;
+
+                if (value != -1)
+                {
+                    cell.Value = _keyValue;
+                }
+            }
+        }
+
+        private void dataGridView1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (dataGridView1.SelectedCells.Count > 1)
+            {
+                _checkKeyValue = true;
+                _keyValue = (int)e.KeyValue;
+                dataGridView1.BeginEdit(false);
+            }
+        }
+
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewCell cell = dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex];
+            foreach (DataGridViewCell cells in dataGridView1.SelectedCells)
+            {
+                dataGridView1.Rows[cells.RowIndex].Cells[cells.ColumnIndex].Value = cell.Value;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
